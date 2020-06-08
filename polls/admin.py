0d965ca390a66f312da91b10938342100acc6b35
Dipendra Subedi <dipesube@gmail.com>
2020-06-08 15:56:31 -0400

    added backend functionality to edit existing polls

--- polls/admin.py
+++ polls/admin.py
@@ -3,4 +3,21 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
+
+admin.site.site_header = "Poll Admin"
+admin.site.site_title = "Poll Admin Area"
+admin.site.index_title = "Welcome"
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields' : ['question_text']}), 
+    ('Date Information', {'fields': ['pub_date'], 'classes':
+    ['collapse']}), ]
+
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
